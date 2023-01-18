1e5e214c19b4a1aa9eb33aca1d7d7b62ccc3b6da
Bryandlc654 <bdelacruz654@gmail.com>
2023-01-18 01:34:29 -0500

    Actualizacion del Incio

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./home.css";
+
+const Social = () => {
+
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/bryan_dlc99/" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a href="https://github.com/Bryandlc654" className="home__social-icon" target="_blank">
+      <i className="uil uil-github-alt"></i>
+      </a>
+      <a href="https://github.com/Bryandlc654" className="home__social-icon" target="_blank"> {/* link de whatsapp */}
+      <i className="uil uil-whatsapp"></i>
+      </a>
+    </div>
+
+  );
+};
+
+export default Social;
